ce09cc969cfc87b4f2a04adf877447f77d5b3708
KonzernTechie <kv.nijesh@gmail.com>
2021-05-20 23:01:43 +0530

    initial commit

---
+++ grpcsample/src/test/java/com/belazy/grpcsample/GrpcsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belazy.grpcsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
